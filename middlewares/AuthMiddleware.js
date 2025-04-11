5fae8a82de60b48b68026aef5c05512e8f9e1084
Riddhi-88 <riddhishukla2003@gmail.com>
2025-04-12 01:38:35 +0530

    Add files via upload

---
+++ middlewares/AuthMiddleware.js
@@ -0,0 +1,16 @@
+
+import jwt from "jsonwebtoken";
+
+
+export const verifyToken = (request, response,next)=>{
+    console.log(request.cookies)
+    const token= request.cookies.jwt;
+  if(!token) return response.status(401).send("you are not authenticated")
+    jwt.verify(token,process.env.JWT_KEY,async(err,payload)=>{
+if(err) return response.status(403).send("Token not is valid");
+request.userId = payload.userId;
+next();
+
+    }
+)
+}
---
+++ routes/ContactRoutes.js
@@ -0,0 +1,10 @@
+import {Router} from "express";
+import { getAllContacts, getContactsForDMList, searchContacts } from "../controllers/ContactController.js";
+import{verifyToken} from "../middlewares/AuthMiddleware.js"
+
+const contactsRoutes = Router();
+contactsRoutes.post("/search",verifyToken,searchContacts);
+contactsRoutes.get("/get-contacts-for-dm",verifyToken,getContactsForDMList)
+contactsRoutes.get("/get-all-contacts",verifyToken,getAllContacts)
+
+export default contactsRoutes;
